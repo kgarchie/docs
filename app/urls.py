0f36be6d33467819135450966ec59d9044dca313
kgarchie <archiethebig@gmail.com>
2022-05-01 14:28:55 +0300

    tried seo 2

--- app/urls.py
+++ app/urls.py
@@ -5,11 +5,11 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('doc/<int:id>/', views.doc, name='doc'),
+    path('doc/<int:id>', views.doc, name='doc'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register, name='register'),
     path('new/', views.add_doc, name='add_doc'),
-    path('delete/<int:id>/', views.delete, name='delete'),
-    path('edit/<int:id>/', views.edit, name='edit'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('edit/<int:id>', views.edit, name='edit'),
 ]
